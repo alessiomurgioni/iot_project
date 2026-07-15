ab2d0109c968c7f1e190a5cf21ab9f93642317c3
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2026-07-15 14:33:36 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ STALE_AFTER_S = int(os.environ.get("STALE_AFTER_S", "45"))
 # Outgoing email for fire-alarm alerts
 SMTP_HOST = os.environ.get("SMTP_SERVER", "smtp.gmail.com")
 SMTP_PORT = os.environ.get("SMTP_PORT", 587)
-SMTP_USERNAME = os.environ.get("SMTP_USER", "NetwatchHomeController@gmail.com")
-SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD", "rlal hxxq hzur qrat")
+SMTP_USERNAME = os.environ.get("SMTP_USER", "mail@gmail.com")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD", "inserire password qui")
 SMTP_USE_TLS = True
 ALERT_FROM_EMAIL = os.environ.get("SMTP_USER", "NetwatchHomeController@gmail.com")
